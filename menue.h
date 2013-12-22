dc6e6d678186eb04dd0940b9ee4ed9d91c0af065
1796843639@qq.com <Striver@localhost.localdomain>
2013-12-22 16:41:18 +0800

    菜单的头文件

---
+++ menue.h
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  menue.h
+ *
+ *    Description: 菜单
+ *
+ *        Version:  1.0
+ *        Created:  2013年12月16日 13时53分59秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  cuijiaojiao, cuijiaojiaofendou@gmail.com
+ *        Company:  Class 1204 of Software
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void menue()
+{
+	printf("---------------------------------------------------\n");
+	printf("\t\t1.绘图\n");
+	printf("\t\t2.显示图中所有景点的信息:\n");
+	printf("\t\t3.你想知道的景点\n");
+	printf("\t\t4.从某个顶点出发的所有顶点的行走方案\n");
+	printf("\t\t5.任意两点之间的一条最短的简单路径\n");
+	printf("\t\t6.从某景点出发给出访问所有景点的最佳行走方案\n");
+	printf("\t\t7.地图的更新\n");
+	printf("\t\t8.查询任意两个景点之间的所有简单路径\n");
+	printf("\t\t9.菜单\n");
+	printf("\t\t0.退出\n");
+}
+
